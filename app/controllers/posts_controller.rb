a8944cf3e4d3d1d25bccb8a9f22a366f697ad9b5
Seyfeddin Bassarac <seyfoyun@gmail.com>
2011-06-07 17:20:29 +0300

    fixes rss bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   
   def index
     @posts = Post.all(:order => "created_at desc")
+    @feedposts = Post.all(:select => "title, body, created_at", :order => "created_at DESC", :limit => 20)
 
     respond_to do |format|
       format.html # index.html.erb
